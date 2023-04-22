26270762ae4d7a6c19ba45847f3270f6f0ca59fa
Matheus Souza <matheus-oliveira17@hotmail.com>
2023-04-22 20:49:12 -0300

    [Add] Diretiva *ngFor.

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { DiretivaNgifComponent } from "./diretiva-ngif/diretiva-ngif.component";
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
-  declarations: [AppComponent, DiretivaNgifComponent],
+  declarations: [AppComponent, DiretivaNgifComponent, DiretivaNgswitchComponent, DiretivaNgforComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
