ce0859f1e382edeba679a67042a8782462d1d1f4
Matheus Souza <matheus-oliveira17@hotmail.com>
2023-04-23 15:41:37 -0300

    [Add] Diretiva [ngStyle].

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { DiretivaNgifComponent } from "./diretiva-ngif/diretiva-ngif.component";
-import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
-import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
-import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgswitchComponent } from "./diretiva-ngswitch/diretiva-ngswitch.component";
+import { DiretivaNgforComponent } from "./diretiva-ngfor/diretiva-ngfor.component";
+import { DiretivaNgclassComponent } from "./diretiva-ngclass/diretiva-ngclass.component";
+import { DiretivaNgstyleComponent } from "./diretiva-ngstyle/diretiva-ngstyle.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, DiretivaNgifComponent, DiretivaNgswitchComponent, DiretivaNgforComponent, DiretivaNgclassComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent,
+    DiretivaNgstyleComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
