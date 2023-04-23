2cfa1c3bee2c28b40ceba7646303573211b9954c
Matheus Souza <matheus-oliveira17@hotmail.com>
2023-04-23 20:58:45 -0300

    [Add] Criando um serviço.

---
+++ servicos/src/app/cursos/cursos.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+
+import { CursosService } from "./cursos.service";
+
+@Component({
+  selector: "app-cursos",
+  templateUrl: "./cursos.component.html",
+  styleUrls: ["./cursos.component.css"],
+})
+export class CursosComponent implements OnInit {
+  private _cursosService: CursosService;
+  constructor() {
+    this._cursosService = new CursosService();
+  }
+
+  ngOnInit(): void {
+    this.listaCursos = this._cursosService.getCursos();
+  }
+
+  listaCursos: string[] = [];
+}
