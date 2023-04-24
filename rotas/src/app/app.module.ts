513035bb95e72e2751aabc57c9483d5efba12cb5
Matheus Souza <matheus-oliveira17@hotmail.com>
2023-04-24 13:33:16 -0300

    [Add] Componentes e configurando rotas simples.

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSliderModule } from "@angular/material/slider";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { CursosComponent } from "./components/cursos/cursos.component";
+import { routing } from "./app.routing";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule],
+  declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule, routing],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { CursosComponent } from "./components/cursos/cursos.component";
+import { ModuleWithProviders } from "@angular/core";
+
+const APP_ROUTES: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "cursos", component: CursosComponent },
+];
+
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(APP_ROUTES);
