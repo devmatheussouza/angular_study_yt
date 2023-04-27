923cfe64b2418b716ca327b543d5156b73f62c19
Matheus Souza <matheus-oliveira17@hotmail.com>
2023-04-27 09:26:32 -0300

    [Change] Refatoração por meio de módulos de rotas.

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ModuleWithProviders } from "@angular/core";
 
 import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from "./components/login/login.component";
-import { CursosComponent } from "./components/cursos/cursos.component";
-import { CursoDetalheComponent } from "./components/cursos/curso-detalhe/curso-detalhe.component";
-import { CursoNaoEncontradoComponent } from "./components/cursos/curso-nao-encontrado/curso-nao-encontrado.component";
+// import { CursosComponent } from "./components/cursos/cursos.component";
+// import { CursoDetalheComponent } from "./components/cursos/curso-detalhe/curso-detalhe.component";
+// import { CursoNaoEncontradoComponent } from "./components/cursos/curso-nao-encontrado/curso-nao-encontrado.component";
 
 const APP_ROUTES: Routes = [
   { path: "", component: HomeComponent },
   { path: "login", component: LoginComponent },
-  { path: "cursos", component: CursosComponent },
-  { path: "curso/:id", component: CursoDetalheComponent },
-  { path: "naoEncontrado", component: CursoNaoEncontradoComponent },
+  // { path: "cursos", component: CursosComponent },
+  // { path: "curso/:id", component: CursoDetalheComponent },
+  // { path: "naoEncontrado", component: CursoNaoEncontradoComponent },
 ];
 
 @NgModule({
--- rotas/src/app/components/cursos/cursos.module.ts
+++ rotas/src/app/components/cursos/cursos.module.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from "@angular/common";
-import { RouterModule, Route } from "@angular/router";
 import { NgModule } from "@angular/core";
 
 import { CursosComponent } from "./cursos.component";
 import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
 import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
 import { CursoService } from "src/app/services/curso.service";
+import { CursosRoutingModule } from "./cursos.routing.module";
 
 @NgModule({
   declarations: [CursosComponent, CursoDetalheComponent, CursoNaoEncontradoComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, CursosRoutingModule],
   exports: [],
   providers: [CursoService],
 })
