43d95df5d75bbe32d9a2bbb4ba0c124624532abf
Matheus <matheus@unisistemas.com.br>
2023-04-28 11:55:13 -0300

    [Add] Componentes e configuração de rotas.

--- forms/src/app/app-routing.module.ts
+++ forms/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataFormComponent } from './components/data-form/data-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'templateForm', component: TemplateFormComponent },
+  { path: 'dataForm', component: DataFormComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PaginaNaoEncontradaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
+import { DataFormComponent } from './components/data-form/data-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TemplateFormComponent, DataFormComponent, HomeComponent, PaginaNaoEncontradaComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
