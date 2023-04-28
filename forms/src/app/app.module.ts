f4bee7f44794b1e1570338a270402f5a7982b0be
Matheus <matheus@unisistemas.com.br>
2023-04-28 12:24:45 -0300

    [Add] Controle do Form com ngForm, ngSubmit e ngModel.

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './components/template-form/template-form.component';
 import { DataFormComponent } from './components/data-form/data-form.component';
 import { HomeComponent } from './components/home/home.component';
 import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
 @NgModule({
-  declarations: [AppComponent, TemplateFormComponent, DataFormComponent, HomeComponent, PaginaNaoEncontradaComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    DataFormComponent,
+    HomeComponent,
+    PaginaNaoEncontradaComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- forms/src/app/components/template-form/template-form.component.ts
+++ forms/src/app/components/template-form/template-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
-  styleUrls: ['./template-form.component.scss']
+  styleUrls: ['./template-form.component.scss'],
 })
 export class TemplateFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
   }
-
 }
