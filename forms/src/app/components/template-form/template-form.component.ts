6ab3ed4e16457c13db21bce6912556fdfd6306a9
Matheus <matheus@unisistemas.com.br>
2023-05-03 11:26:48 -0300

    [Add] Diretiva ngModelGroup.

--- forms/src/app/components/template-form/template-form.component.ts
+++ forms/src/app/components/template-form/template-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, NgModelGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -7,11 +7,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./template-form.component.scss'],
 })
 export class TemplateFormComponent implements OnInit {
+  @ViewChild('endereco') endereco: NgModelGroup;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   onSubmitForm(form: NgForm) {
     // console.log(form.valid);
+    console.log(
+      `NgModelGroup (Endereço): ${JSON.stringify(
+        this.endereco.value,
+        null,
+        ' '
+      )}`
+    );
   }
 }
