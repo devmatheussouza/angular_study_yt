453474317bdec65660fd7b15f9732d192b4f4e60
Matheus <matheus@unisistemas.com.br>
2023-05-15 11:49:56 -0300

    [Add] Validação nos campos.

--- forms/src/app/components/data-form/data-form.component.ts
+++ forms/src/app/components/data-form/data-form.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -14,21 +14,36 @@ export class DataFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null],
+      nome: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100),
+        ],
+      ],
+      email: [null, [Validators.required, Validators.email]],
     });
   }
 
   onSubmit(): void {
-    this.http
-      .post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-      .subscribe({
-        next: (res: any) => {
-          console.log(res);
-          this.resetarForm();
-        },
-        error: (error: any) => console.error(error),
+    if (this.formulario.valid) {
+      this.http
+        .post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+        .subscribe({
+          next: (res: any) => {
+            console.log(res);
+            this.resetarForm();
+          },
+          error: (error: any) => console.error(error),
+        });
+    } else {
+      Object.entries(this.formulario.controls).forEach((entry) => {
+        if (entry[1].status === 'INVALID') {
+          console.error(`${entry[0]}: ${entry[1].status}`);
+        }
       });
+    }
   }
 
   protected resetarForm(): void {
